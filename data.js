a4df98d2b329623c3029e1ed0f17ff8408df6a9b
id141365154 <id141365154@gmail.com>
2019-02-23 12:58:01 +0300

    separate data obj from main.js

---
+++ data.js
@@ -0,0 +1,41 @@
+const input = {
+    p1: {
+        s1: {
+            title: 'scene 1',
+            description: 'description 1',
+            value: 1,
+        },
+        s2: {
+            title: 'scene 2',
+            description: 'description 2',
+            value: 32,
+        },
+        s3: {
+            title: 'scene 3',
+            description: 'description 3',
+            value: 89,
+        },
+        s4: {
+            title: 'scene 3',
+            description: 'description 3',
+            value: 0,
+        },
+    },
+    p2: {
+        s5: {
+            title: 'scene 5',
+            description: 'description 5',
+            value: 0,
+        },
+        s6: {
+            title: 'scene 6',
+            description: 'description 6',
+            value: 42,
+        },
+        s7: {
+            title: 'scene 7',
+            description: 'description 7',
+            value: -9,
+        },
+    },
+}
--- main.js
+++ main.js
@@ -1,45 +1,8 @@
-const input = {
-    p1: {
-        s1: {
-            title: 'scene 1',
-            description: 'description 1',
-            value: 1,
-        },
-        s2: {
-            title: 'scene 2',
-            description: 'description 2',
-            value: 32,
-        },
-        s3: {
-            title: 'scene 3',
-            description: 'description 3',
-            value: 89,
-        },
-        s4: {
-            title: 'scene 3',
-            description: 'description 3',
-            value: 0,
-        },
-    },
-    p2: {
-        s5: {
-            title: 'scene 5',
-            description: 'description 5',
-            value: 0,
-        },
-        s6: {
-            title: 'scene 6',
-            description: 'description 6',
-            value: 42,
-        },
-        s7: {
-            title: 'scene 7',
-            description: 'description 7',
-            value: -9,
-        },
-    },
-}
+//input declared in data.js
 
+var output = normilize(input, true);
+
+console.log(output);
 
 function normilize(obInput, sort) {
     var res = [];
@@ -74,7 +37,3 @@ function normilize(obInput, sort) {
         this.title = String(obData.title);
     };
 }
-
-var output = normilize(input, true);
-
-console.log(output);
